a7bcaa811e488e01fe9482b715eaeb14829bdad8
Angelo <filannino.angelo@gmail.com>
2017-11-28 12:47:55 +0100

    Minor update
    - Added comments

--- src/it/polito/verigraph/grpc/client/ToscaClientGrpcUtils.java
+++ src/it/polito/verigraph/grpc/client/ToscaClientGrpcUtils.java
@@ -5,97 +5,96 @@ import it.polito.verigraph.tosca.classes.*;
 import it.polito.verigraph.grpc.tosca.*;
 import it.polito.verigraph.grpc.tosca.NodeTemplateGrpc.Type;
 
-
 public class ToscaClientGrpcUtils {
 	
-	/** Default configuration for a Tosca node non compliant with Verigraph types*/
+	/** Default configuration for a Tosca NodeTemplate non compliant with Verigraph types*/
 	public static final String defaultConfID = new String("0");
 	public static final String defaultDescr = new String("Default Configuration");
-	public static final String defaultConfig = new String("");//add endhost default configuration
+	public static final String defaultConfig = new String("");
 	
 	/** Helper for Client interface*/
 	public static final String helper = new String("");
-	
-	
-	
 
-    /** Method for parsing a tosca Node into a Grpc Node */
-    public static NodeTemplateGrpc parseNodeTemplate(TNodeTemplate toscaNode)
-    	throws ClassCastException{
-    	
+    /** Parsing method: TNodeTemplate(tosca) --> NodeTemplateGrpc */
+    public static NodeTemplateGrpc parseNodeTemplate(TNodeTemplate toscaNode) throws ClassCastException {   	
     	Boolean isVerigraphCompl = true;
     	
-    	//Start building our node
-    	NodeTemplateGrpc.Builder parsed = NodeTemplateGrpc.newBuilder();
-    	parsed.setId(Long.valueOf(toscaNode.getId()).longValue()); //to convert
-    	parsed.setName(toscaNode.getName());
-
-    	//In case our node is not tosca compliant, we assume it to be an endhost node
+    	//NodeTemplateGrpc building
+    	NodeTemplateGrpc.Builder parsed = NodeTemplateGrpc.newBuilder()
+    			.setId(Long.valueOf(toscaNode.getId()).longValue()) //longValue is used to convert in long
+    			.setName(toscaNode.getName());
     	switch(toscaNode.getType().getLocalPart().toLowerCase()) {
     		case "antispam":
     			parsed.setType(Type.antispam);
+    			break;
     		case "cache":
     			parsed.setType(Type.cache);
+    			break;
     		case "dpi":
     			parsed.setType(Type.dpi);
+    			break;
     		case "endhost":
     			parsed.setType(Type.endhost);
+    			break;
     		case "endpoint":
     			parsed.setType(Type.endpoint);
+    			break;
     		case "fieldmodifier":
     			parsed.setType(Type.fieldmodifier);
+    			break;
     		case "firewall":
     			parsed.setType(Type.firewall);
+    			break;
     		case "mailclient":
     			parsed.setType(Type.mailclient);
+    			break;
     		case "mailserver":
     			parsed.setType(Type.mailserver);
+    			break;
     		case "nat":
     			parsed.setType(Type.nat);
+    			break;
     		case "vpnaccess":
     			parsed.setType(Type.vpnaccess);
+    			break;
     		case "vpnexit":
     			parsed.setType(Type.vpnexit);
+    			break;
     		case "webclient":
     			parsed.setType(Type.webclient);
+    			break;
     		case "webserver":
     			parsed.setType(Type.webserver);
-    		default:
+    			break;
+    		default: //in case the NodeTemplate is not TOSCA-Verigraph compliant, we assume it to be an endhost node
     			parsed.setType(Type.endhost);
     			isVerigraphCompl = false;
-    	}
-    	
-    	
-    	ToscaConfigurationGrpc grpcConfig;
-    	
+    			break;
+    	}  	
+    	ToscaConfigurationGrpc grpcConfig;   	
     	if(isVerigraphCompl) {
     		TConfiguration nodeConfig = ((TConfiguration)toscaNode.getProperties().getAny());
         	grpcConfig = ToscaConfigurationGrpc.newBuilder()
    			 	 .setId(nodeConfig.getConfID())
    				 .setDescription(nodeConfig.getConfDescr())
    			     .setConfiguration(nodeConfig.getJSON()).build();
-    	}else {
+    	} else {
         	grpcConfig = ToscaConfigurationGrpc.newBuilder()
    			 	 .setId(ToscaClientGrpcUtils.defaultConfID)
    				 .setDescription(ToscaClientGrpcUtils.defaultDescr)
    			     .setConfiguration(ToscaClientGrpcUtils.defaultConfig).build();
-    	}
-	
+    	}	
     	parsed.setConfiguration(grpcConfig);
-    	return parsed.build();
+    	return parsed.build();   
+    }  
     
-    }
-	
-    
-    
-    /** Cast a tosca RelationshipTemplate into a grpc RelationshipTemplate */
-    public static RelationshipTemplateGrpc parseRelationshipTemplate(TRelationshipTemplate toscaRel)
-    	throws ClassCastException{
+    /** Parsing method: TRelationshipTemplate(tosca) --> RelationshipTemplateGrpc */
+    public static RelationshipTemplateGrpc parseRelationshipTemplate(TRelationshipTemplate toscaRel) throws ClassCastException{   	
     	
-    	RelationshipTemplateGrpc.Builder parsed = RelationshipTemplateGrpc.newBuilder();
-    	
-    	parsed.setId(Long.valueOf(toscaRel.getId()).longValue())
-    		.setName(toscaRel.getName());
+    	//NodeTemplateGrpc building
+    	RelationshipTemplateGrpc.Builder parsed = RelationshipTemplateGrpc.newBuilder()  	
+    			.setId(Long.valueOf(toscaRel.getId()).longValue())
+    			.setName(toscaRel.getName());
     	
     	String source = ((QName)toscaRel.getSourceElement().getRef()).getLocalPart();
     	String target = ((QName)toscaRel.getTargetElement().getRef()).getLocalPart();
